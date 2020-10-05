f31bee6f3b15f371763c2bbbb714c2fcc0145527
monroe-git <shin.hyunsoo.sub@gmail.com>
2020-10-05 10:42:19 +0900

    vimrc, tmux, bash

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc $HOME/
+
+# Oh-My-Bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+# Oh-My-Tmux
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
